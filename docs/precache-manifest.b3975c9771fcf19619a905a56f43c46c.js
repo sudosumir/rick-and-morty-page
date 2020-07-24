ea69c4c99d5075e2e3131e42862401f954f158c9
oceballos <oceballosmi@outlook.es>
2020-07-23 21:56:01 -0500

    docs agregado.

---
+++ docs/precache-manifest.b3975c9771fcf19619a905a56f43c46c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba1b956b47dc1d0ce9ad902ee8525cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aeee8161c7dfc94393e",
+    "url": "/static/css/main.9b9d02e0.chunk.css"
+  },
+  {
+    "revision": "eaaae333a76c7f5978e8",
+    "url": "/static/js/2.05d5f3ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.05d5f3ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aeee8161c7dfc94393e",
+    "url": "/static/js/main.00bef229.chunk.js"
+  },
+  {
+    "revision": "104e63db1513dddb4014",
+    "url": "/static/js/runtime-main.79a519be.js"
+  }
+]);
